4c1fd116aa6ae9c64eddc42bee1c5e1f1b0f7ad4
Arijeet De <arijeetde@gmail.com>
2023-12-05 12:13:17 +0530

    POST and DELETE request

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,13 +1,22 @@
+let bodyParser = require('body-parser');
+
+let data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
+let urlencodedParser = bodyParser.urlencoded({extended: false});
+
 module.exports = function (app) {
     app.get('/todo', (req, res) => {
-        res.render('todo');
+        res.render('todo', {todos: data});
     });
 
-    app.post('/todo', (req, res) => {
-
+    app.post('/todo', urlencodedParser, (req, res) => {
+        data.push(req.body);
+        res.json(data);
     });
 
-    app.delete('/delete', (req, res) => {
-
+    app.delete('/todo/:item', (req, res) => {
+        data = data.filter((todo) => {
+            return todo.item.replace(/ /g, '-') !== req.params.item;
+        });
+        res.json(data);
     });
 }
